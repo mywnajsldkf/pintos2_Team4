fdec6b548097107b207b109cbf5b6bc261b78a90
mywnajsldkf <mywnajsldkf@gmail.com>
2023-06-23 02:44:15 +0000

    ✨ feat : supplemental_page_table_kill #4
    - hash_clear 함수를 이용해서 spt의 공간을 free 해준다.

--- vm/vm.c
+++ vm/vm.c
@@ -349,6 +349,7 @@ void
 supplemental_page_table_kill (struct supplemental_page_table *spt UNUSED) {
     /* TODO: Destroy all the supplemental_page_table hold by thread and
      * TODO: writeback all the modified contents to the storage. */
+    hash_clear(&spt->spt_hash, clear_spt);  // hash_destroy vs hash_clear
 }
 
 /**
